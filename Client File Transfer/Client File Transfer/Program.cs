a1316bb535fd7f30cc90d73f44d2d1a693c40b93
ilkayk37 <ilkaykurtaran37@gmail.com>
2024-12-08 03:24:14 +0300

    dersim

---
+++ Client File Transfer/Client File Transfer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_File_Transfer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverIp = "45.94.170.239";
+            int port = 9000; 
+            string filePath = "C:\\Users\\Developer\\Desktop\\Gonderilecek Dosya";
+
+
+
+            try
+            {
+                if (Directory.Exists(filePath))
+                {
+                    Console.WriteLine($"klasör doğru {filePath}");
+                    foreach (var file in Directory.GetFiles(filePath))
+                    {
+                        using (TcpClient client = new TcpClient(serverIp, port))
+                        {
+                            Console.WriteLine("Sunucuya bağlanıldı");
+                            using (NetworkStream stream = client.GetStream())
+                            {
+                                string fileName = Path.GetFileName(file);
+                                byte[] fileNameBuffer = System.Text.Encoding.UTF8.GetBytes(fileName);
+                                byte[] fileNameLengthBuffer = BitConverter.GetBytes(fileNameBuffer.Length);
+
+                                stream.Write(fileNameLengthBuffer, 0, fileNameLengthBuffer.Length);
+                                stream.Write(fileNameBuffer, 0, fileNameBuffer.Length);
+
+
+                                using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+                                {
+                                    fs.CopyTo(stream);
+                                    Console.WriteLine($"Dosya başarıyla gönderild {file}");
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Böyle klasör yok");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Server File Transfer Code/Server File Transfer Code/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+try
+{
+    int port = 9000;
+    TcpListener listener = new TcpListener(IPAddress.Any, port);
+    listener.Start();
+    Console.WriteLine("sunucu bağlandı ve port dinleniyor");
+
+    while (true)
+    {
+        using (TcpClient client = listener.AcceptTcpClient())
+        {
+            Console.WriteLine("bir client bağlandı");
+            using (NetworkStream ns = client.GetStream())
+            {
+               
+                byte[] fileNameLengthBuffer = new byte[4];
+                ns.Read(fileNameLengthBuffer, 0, fileNameLengthBuffer.Length);
+                int fileNameLength = BitConverter.ToInt32(fileNameLengthBuffer, 0);
+
+                byte[] fileNameBuffer = new byte[fileNameLength];
+                ns.Read(fileNameBuffer, 0, fileNameBuffer.Length);
+                string fileName = System.Text.Encoding.UTF8.GetString(fileNameBuffer);
+
+                
+                using (FileStream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    ns.CopyTo(stream);
+                    Console.WriteLine("Dosya başarıyla kaydedildi");
+                }
+            }
+        }
+        
+
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
